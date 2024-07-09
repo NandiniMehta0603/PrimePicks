f0b4bce7d94c097bae4be55179eed92e668fa412
Nandini <mehtanandini684@gmail.com>
2024-07-09 10:43:18 +0530

    First commit of base project

---
+++ src/test/java/com/movie/primepicks/PrimePicksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.primepicks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimePicksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
